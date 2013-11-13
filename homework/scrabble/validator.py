cb05b13f79712ecb51f027982c17c7601d9a7a70
Naomi Seyfer <naomi@seyfer.org>
2013-11-12 18:46:17 -0800

    scrabble assignment

---
+++ homework/scrabble/validator.py
@@ -0,0 +1,13 @@
+import board
+import samples
+
+def validate(d):
+    """
+    Returns True if the board is a valid Scrabble state, False if not.
+
+    d must be a board in the list-of-lists-of-dictionaries format.
+    """
+    pass
+
+
+#remember to write helper functions!
